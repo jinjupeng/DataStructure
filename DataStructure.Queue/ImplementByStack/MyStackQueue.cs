52e43db8619fb8d9dc5364d847e863678196e2dd
jinjupeng <im.jp@outlook.com>
2020-08-19 08:59:57 +0800

    数据结构之队列

---
+++ DataStructure.Queue/ImplementByStack/MyStackQueue.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DataStructure.Queue.ImplementByStack
+{
+    /// <summary>
+    /// 用栈实现队列
+    /// </summary>
+    public class MyStackQueue
+    {
+        private readonly Stack<int> _stack;
+        /** Initialize your data structure here. */
+        public MyStackQueue()
+        {
+            _stack = new Stack<int>();
+        }
+
+        /** Push element x to the back of queue. */
+        public void Push(int x)
+        {
+            _stack.Push(x);
+
+        }
+
+        /** Removes the element from in front of queue and returns that element. */
+        public int Pop()
+        {
+            var array = _stack.ToArray().Reverse().ToArray();
+            var p = (int)array[0];
+            _stack.Clear();
+            for (int i = 1; i < array.Length; i++)
+            {
+                _stack.Push(array[i]);
+            }
+            return p;
+        }
+
+        /** Get the front element. */
+        public int Peek()
+        {
+            var array = _stack.ToArray().Reverse().ToArray();
+            return (int)array[0];
+        }
+
+        /** Returns whether the queue is empty. */
+        public bool Empty()
+        {
+            return _stack.Count == 0;
+        }
+    }
+}
---
+++ DataStructure.Stack/ImplementByQueue/MyQueueStack.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructure.Stack.ImplementByQueue
+{
+    /// <summary>
+    /// 用队列实现栈
+    /// </summary>
+    public class MyQueueStack
+    {
+        private readonly Queue<int> _inputQueue;
+        /** Initialize your data structure here. */
+        public MyQueueStack()
+        {
+            _inputQueue = new Queue<int>();
+        }
+
+        /** Push element x onto stack. */
+        public void Push(int x)
+        {
+            _inputQueue.Enqueue(x);
+        }
+
+        /** Removes the element on top of the stack and returns that element. */
+        public int Pop()
+        {
+            var size = _inputQueue.Count - 1;
+            for (int i = 0; i < size; i++)
+            {
+                var data = _inputQueue.Peek();
+                _inputQueue.Dequeue();
+                _inputQueue.Enqueue(data);
+            }
+
+            return _inputQueue.Dequeue();
+        }
+
+        /** Get the top element. */
+        public int Top()
+        {
+            var size = _inputQueue.Count - 1;
+            for (int i = 0; i < size; i++)
+            {
+                var data = _inputQueue.Peek();
+                _inputQueue.Dequeue();
+                _inputQueue.Enqueue(data);
+            }
+
+            var value = _inputQueue.Peek();
+            _inputQueue.Dequeue();
+            _inputQueue.Enqueue(value);
+            return value;
+        }
+
+        /** Returns whether the stack is empty. */
+        public bool Empty()
+        {
+            return _inputQueue.Count == 0;
+        }
+    }
+}
