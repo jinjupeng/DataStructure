f75ac7771f309b715962f85e8b96c7743a5abc85
jinjupeng <im.jp@outlook.com>
2020-09-03 16:16:44 +0800

    用数组实现循环队列、动态数组实现队列

--- DataStructure.Queue/ImplementByArray/MyDynamicArrayQueue.cs
+++ DataStructure.Queue/ImplementByArray/MyDynamicArrayQueue.cs
@@ -1,10 +1,70 @@
-﻿namespace DataStructure.Queue.ImplementByArray
+﻿using System;
+
+namespace DataStructure.Queue.ImplementByArray
 {
     /// <summary>
     /// 动态数组实现队列
     /// </summary>
     public class MyDynamicArrayQueue
     {
-        
+        // 数组：items，数组大小：n
+        private string[] items;
+        private int n = 0;
+
+        // head表示队头下标，tail表示队尾下标
+        private int _head = 0;
+        private int _tail = 0;
+
+        // 申请一个大小为capacity的数组
+        public MyDynamicArrayQueue(int capacity)
+        {
+            items = new string[capacity];
+            n = capacity;
+        }
+
+        // 入队操作，将item放入队尾
+        public bool Enqueue(string item)
+        {
+            // tail == n表示队列末尾没有空间了
+            if (_tail == n)
+            {
+                // tail ==n && head==0，表示整个队列都占满了
+                if (_head == 0) return false;
+                // 数据搬移
+                for (int i = _head; i < _tail; ++i)
+                {
+                    items[i - _head] = items[i];
+                }
+                // 搬移完之后重新更新head和tail
+                _tail -= _head;
+                _head = 0;
+            }
+
+            items[_tail] = item;
+            _tail++;
+            return true;
+        }
+
+        /// <summary>
+        /// 出队
+        /// </summary>
+        /// <returns></returns>
+        public string Dequeue()
+        {
+            // 如果head == tail 表示队列为空
+            if (_head == _tail) return null;
+            string ret = items[_head];
+            ++_head;
+            return ret;
+        }
+
+        public void PrintAll()
+        {
+            for (int i = _head; i < _tail; ++i)
+            {
+                Console.WriteLine(items[i] + " ");
+            }
+            Console.ReadLine();
+        }
     }
 }
--- DataStructure.Queue/MyCircularQueue.cs
+++ DataStructure.Queue/MyCircularQueue.cs
@@ -1,10 +1,63 @@
-﻿namespace DataStructure.Queue
+﻿using System;
+
+namespace DataStructure.Queue
 {
     /// <summary>
-    /// 循环队列
+    /// 数组实现循环队列
     /// </summary>
     public class MyCircularQueue
     {
-        
+        // 数组：items，数组大小：n
+        private string[] items;
+        private int n = 0;
+
+        // head表示队头下标，tail表示队尾下标
+        private int _head = 0;
+        private int _tail = 0;
+
+        // 申请一个大小为capacity的数组
+        public MyCircularQueue(int capacity)
+        {
+            items = new string[capacity];
+            n = capacity;
+        }
+
+        /// <summary>
+        /// 入队
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool Enqueue(string item)
+        {
+            // 队列满了
+            if ((_tail + 1) % n == _head) return false;
+            items[_tail] = item;
+            _tail = (_tail + 1) % n;
+            return true;
+        }
+
+        /// <summary>
+        /// 出队
+        /// </summary>
+        /// <returns></returns>
+        public string Dequeue()
+        {
+            // 如果head == tail 表示队列为空
+            if (_head == _tail) return null;
+            string ret = items[_head];
+            _head = (_head + 1) % n;
+            return ret;
+        }
+
+        public void PrintAll()
+        {
+            if (0 == n) return;
+            for (int i = _head; i % n != _tail; i = (i + 1) % n)
+            {
+                Console.WriteLine(items[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
     }
 }
