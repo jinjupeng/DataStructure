583d2f69d9ce9e2d84d14cfe8f97d883da7907a9
jinjupeng <im.jp@outlook.com>
2020-09-03 11:37:20 +0800

    有序数组的二分查找

---
+++ DataStructure.Array/BinarySearchBySortedArray/BinarySearchImpl.cs
@@ -0,0 +1,70 @@
+﻿namespace DataStructure.Array.BinarySearchBySortedArray
+{
+    public class BinarySearchImpl
+    {
+        /// <summary>
+        /// 二分查找普通循环实现
+        /// </summary>
+        /// <param name="sortedArray">有序的数组</param>
+        /// <param name="key">查找的元素</param>
+        /// <returns>查找元素的下标</returns>
+        public static int BinarySearch(int[] sortedArray, int key)
+        {
+            var low = 0;
+            var high = sortedArray.Length - 1;
+
+            while (low <= high)
+            {
+                var mid = (low + high) / 2;
+                if (sortedArray[mid] == key)
+                {
+                    return mid;
+                }
+
+                if (sortedArray[mid] < key)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// 二分查找递归实现
+        /// </summary>
+        /// <param name="sortedArray">有序数组</param>
+        /// <param name="start">元素下标</param>
+        /// <param name="end">元素下标</param>
+        /// <param name="key">查找的元素值</param>
+        /// <returns>返回查找元素值的下标</returns>
+        public static int BinarySearch(int[] sortedArray, int start, int end, int key)
+        {
+            int mid = start + (end - start) / 2;
+            if (sortedArray[mid] == key)
+            {
+                return mid;
+            }
+            if (start >= end)
+            {
+                return -1;
+            }
+
+            if (key > sortedArray[mid])
+            {
+                return BinarySearch(sortedArray, mid + 1, end, key);
+            }
+
+            if (key < sortedArray[mid])
+            {
+                return BinarySearch(sortedArray, start, mid - 1, key);
+            }
+            return -1;
+
+        }
+    }
+}
--- DataStructure.Array/Program.cs
+++ DataStructure.Array/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DataStructure.Array.BinarySearchBySortedArray;
 
 namespace DataStructure.Array
 {
@@ -6,6 +7,9 @@ namespace DataStructure.Array
     {
         static void Main(string[] args)
         {
+            var array = new int[10] { 1,3,5,6,8,12,14,16,18,20 };
+            // var data = BinarySearchImpl.BinarySearch(array, 5);
+            var data = BinarySearchImpl.BinarySearch(array, 0, 5, 1);
             Console.WriteLine("Hello World!");
         }
     }
