eb85eb5df971619ca3fb8ebb29ad26bc23a79ed0
猫先生 <im.jp@outlook.com>
2021-10-17 14:34:04 +0800

    新增递归二分查找实现

--- src/CSharp/DataStructure.Search/BinarySearch.cs
+++ src/CSharp/DataStructure.Search/BinarySearch.cs
@@ -2,36 +2,64 @@
 {
     public class BinarySearch
     {
-        /// <summary>
-        /// 二分查找（折半查找）
-        /// </summary>
-        /// <param name="nums"></param>
-        /// <param name="target"></param>
-        /// <returns></returns>
-        public int IndexOf(int[] nums, int target)
-        {
-            var low = 0;
-            var high = nums.Length - 1;
+        /**
+         * 当要查找的记录从头到尾都是有序排列的时候，为找到数值可以执行一种比
+         * 顺序查找更加有效的查找。这种查找被称为是二叉查找。
+         * 
+         */
 
+        /// <summary>
+        /// 二分查找While循环实现
+        /// </summary>
+        /// <param name="nums">数组</param>
+        /// <param name="low">开始索引</param>
+        /// <param name="high">结束索引</param>
+        /// <param name="target">要查找的对象</param>
+        /// <returns>返回索引</returns>
+        public static int BinSearch(int[] nums, int low, int high, int target)
+        {
             while (low <= high)
             {
-                var mid = (low + high) / 2;
-                if (nums[mid] == target)
+                int middle = (low + high) / 2;
+                if (target == nums[middle])
                 {
-                    return mid;
+                    return middle;
                 }
-
-                if (nums[mid] < target)
+                else if (target > nums[middle])
                 {
-                    low = mid + 1;
+                    low = middle + 1;
                 }
-                else
+                else if (target < nums[middle])
                 {
-                    high = mid - 1;
+                    high = middle - 1;
                 }
             }
-
             return -1;
+        }
+
+
+        /// <summary>
+        /// 二分查找递归实现
+        /// </summary>
+        /// <param name="arr">数组</param>
+        /// <param name="low">开始索引 0</param>
+        /// <param name="high">结束索引 </param>
+        /// <param name="key">要查找的对象</param>
+        /// <returns>返回索引</returns>
+        public static int RbinSearch(int[] arr, int low, int high, int key)
+        {
+            int mid = (low + high) / 2;//中间索引
+            if (low > high)
+                return -1;
+            else
+            {
+                if (arr[mid] == key)
+                    return mid;
+                else if (arr[mid] > key)
+                    return RbinSearch(arr, low, mid - 1, key);
+                else
+                    return RbinSearch(arr, mid + 1, high, key);
+            }
         }
     }
 }
